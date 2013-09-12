ca70b7c75eedde9594655d8b7328e03700dd3722
Finlay Curran <finlay.curran@gmail.com>
2013-09-12 14:03:05 +0100

    No real changes. But after lots of fiddling I can now embed this in my website :)

--- src/util/Assets.java
+++ src/util/Assets.java
@@ -2,7 +2,6 @@ package util;
 
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.util.ResourceLoader;
 
 public class Assets {
 
@@ -17,12 +16,8 @@ public class Assets {
 		// .getResourceAsStream("http://fcexb.com/test/data/fishSmall.png"),
 		// "http://fcexb.com/test//data/fish sprite.png", false);
 
-		FISH_STILL = new Image(
-				ResourceLoader.getResourceAsStream("/data/fishSmall.png"),
-				"/data/fishSmall.png", false);
+		FISH_STILL = new Image("data/fishSmall.png");
 
-		WATER_SKY_BACKGROUND = new Image(
-				ResourceLoader.getResourceAsStream("/data/waterSky.jpg"),
-				"/data/waterSky.jpg", false);
+		WATER_SKY_BACKGROUND = new Image("data/waterSky.jpg");
 	}
 }
